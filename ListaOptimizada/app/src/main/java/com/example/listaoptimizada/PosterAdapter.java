d91d8ae46abb7746d90c03447881cc9456bc0285
Carlota Maroto <cmg0058@alu.medac.es>
2025-11-13 15:59:08 +0100

    2.5

---
+++ ListaOptimizada/app/src/main/java/com/example/listaoptimizada/PosterAdapter.java
@@ -0,0 +1,62 @@
+package com.example.listaoptimizada;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PostersViewHolder>
+{
+
+    private Posters[] listaPeliculas;
+
+    public PosterAdapter(Posters[] listaPeliculas)
+    {
+        this.listaPeliculas = listaPeliculas;
+    }
+
+    public static class PostersViewHolder extends RecyclerView.ViewHolder {
+        public TextView tituloView;
+        public ImageView portadaView;
+        private Context context;
+        public PostersViewHolder(View view, Context context) {
+            super(view);
+            this.context = context;
+
+            tituloView = view.findViewById(R.id.tituloView);
+            portadaView = view.findViewById(R.id.imagenView);
+        }
+
+        public void BindPosters(Posters posters) {
+            tituloView.setText(posters.getTitle());
+            portadaView.setImageResource(
+                    context.getResources()
+                            .getIdentifier(posters.getImage(),
+                                    "drawable",
+                                    "com.example.ListaOptimizada"));
+        }
+    }
+
+    @NonNull
+    @Override
+    public PostersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.posters_item, parent, false);
+        return new PostersViewHolder(view, parent.getContext());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostersViewHolder holder, int position) {
+        holder.BindPosters(this.listaPosters[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPosters.length;
+    }
+}
---
+++ ListaOptimizada/app/src/main/java/com/example/listaoptimizada/Posters.java
@@ -0,0 +1,17 @@
+package com.example.listaoptimizada;
+
+public class Posters {
+    private String title;
+    private String image;
+    public Posters(String title, String image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getImage() {
+        return image;
+    }
+}
